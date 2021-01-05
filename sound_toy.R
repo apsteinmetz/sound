f2a1e8c4252c918c4e5e728dcd70af12add894ed
apsteinmetz <apsteinmetz@yahoo.com>
2021-01-05 16:04:23 -0500

    sample subset of sound files to make training sets of reasonable size

--- sound_toy.R
+++ sound_toy.R
@@ -10,11 +10,12 @@ library(soundgen)
 vf <- "data/bk_train_voice.mp3"
 mf <- "data/bk_train_music.mp3"
 
-train_voice_raw <- tuneR::readMP3(vf) %>% normalize()
-voice_melfcc <- train_voice_raw %>% melfcc()
+train_voice_raw <- tuneR::readMP3(vf) %>% tuneR::normalize(unit="32")
+play(train_voice_raw)
+#voice_melfcc <- train_voice_raw %>% melfcc()
 
-train_music_raw <- tuneR::readMP3("data/bk_train_music.mp3") %>% normalize()
-
+train_music_raw <- tuneR::readMP3("data/bk_train_music.mp3") %>% tuneR::normalize(unit="32")
+tuneR::play(train_music_raw)
 
 seewave::spectro(train_music_raw, flim = c(0,2),fastdisp = TRUE)
 seewave::spectro(train_voice_raw, flim = c(0,2),fastdisp = TRUE)
@@ -29,17 +30,24 @@ fft_data_m <- read_audio_fft(mf,start_time = 200, end_time = 205)
 readWave()
 
 v_properties <-soundgen::analyze(vf)
-save(v_properties,file="data/v_properties.rdata")
-m_properties <-soundgen::analyze(mf)
-save(m_properties,file="data/m_properties.rdata")
 
 
-# Get 60 seconds of wave in 5 second blocks
-
+# shrink by taking 60 seconds total of wave equally spaced in 5 second blocks
 get_wave_subset <- function(wave_obj,desired_secs = 60,segment_secs = 5){
-   segment_count <- round(desired_secs,segment_secs)
-   start_points <- seq.int(0, length(wave_obj),)
-   for (n in 0:segment_count){
-      segment <- wave_obj[]
-   }
+   segment_count <- round(desired_secs/segment_secs)
+   segment_size <- segment_secs*wave_obj@samp.rate
+   segment_starts <- length(wave_obj)/segment_count
+   start_points <- seq.int(1, length(wave_obj),segment_starts)
+   segments <- unlist(map2(start_points,start_points+segment_size,seq))
+   wave_obj@left <- wave_obj@left[segments]
+   return(wave_obj)
 }
+
+train_voice <- get_wave_subset(train_voice_raw)
+
+v_properites <- analyze(train_voice@left,train_voice@samp.rate)
+save(v_properties,file="data/v_properties.rdata")
+
+train_music <- get_wave_subset(train_music_raw)
+m_properties <- analyze(train_music@left,train_voice@samp.rate)
+save(m_properties,file="data/m_properties.rdata")
